da564ac5396d39d7f58c8a76f439b07c4f27c918
Cole Willsea <coleww@gmail.com>
2014-10-26 12:10:02 -0700

    hopefully removing img from thaaaa gem

--- lib/scrambled_jpeg.rb
+++ lib/scrambled_jpeg.rb
@@ -1,14 +1,18 @@
 require 'scrambled_jpeg/version'
 module ScrambledJpeg
   class << self
-    def scramble filename, grain
+    def scramble filename, grain=5
       ending, cache = (store = File.readlines filename).pop, []
       (new_file = File.open Time.now.hash.to_s + '.jpg', "w").write store.shift
-      (grain || 5).times { new_file.write store.shift }
+      grain.times { new_file.write store.shift }
            .times {       cache << store.shift }
            .times { new_file.write store.shift }
            .times { new_file.write cache.shift } until store.empty? && cache.empty?
       new_file.write ending && new_file.close
+    end
+
+    def hardboil filename, times=1
+
     end
   end
 end
--- scrambled_jpeg.gemspec
+++ scrambled_jpeg.gemspec
@@ -12,7 +12,7 @@ Gem::Specification.new do |spec|
   spec.homepage      = "https://github.com/coleww/scrambled_jpeg"
   spec.license       = "MIT"
 
-  spec.files         = `git ls-files -z`.split("\x0")
+  spec.files         = Dir.glob("{bin,lib}/**/*") + %w(LICENSE.txt README.md Rakefile Gemfile)
   spec.executables   = ['scrambled_jpeg']
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
